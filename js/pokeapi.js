062ea329fde92dbcce39ad6e6fec50fa6581d94f
HaroldPIERRACHE <h.pierrache@gmail.com>
2021-10-27 14:58:39 +0200

    altelier_ajax2

--- js/pokeapi.js
+++ js/pokeapi.js
@@ -1,18 +1,26 @@
-function getPokemons() {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-               let response = JSON.parse(httpRequest.responseText);
-               let ul = document.getElementById("pokelist");
-               for(let pokemon of response.results) {
-                   ul.innerHTML += `<li>${pokemon.name}</li>`
-               }
-            }else {
-
-            }
-        }
-    }
-    httpRequest.open("GET", "https://pokeapi.co/api/v2/pokemon/");
-    httpRequest.send();
+function getPokemons(url = "https://pokeapi.co/api/v2/pokemon/") {
+  fetch(url).then(function(response) {
+       return response.json();
+   }).then(function(response) {
+        let ul = document.getElementById("pokelist");
+          for(let pokemon of response.results) {
+              ul.innerHTML += `<li>${pokemon.name}</li>`
+         }
+   })
 }
+// let httpRequest = new XMLHttpRequest();
+   // httpRequest.onreadystatechange = function() {
+  //      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      //      if (httpRequest.status === 200) {
+     //          let response = JSON.parse(httpRequest.responseText);
+       //        let ul = document.getElementById("pokelist");
+         //      for(let pokemon of response.results) {
+           //        ul.innerHTML += `<li>${pokemon.name}</li>`
+             //  }
+            //}else {
+
+         //   }
+      //  }
+   // }
+    //httpRequest.open("GET", "https://pokeapi.co/api/v2/pokemon/");
+   // httpRequest.send();
